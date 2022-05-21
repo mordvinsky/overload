c1ce1652941f0dd5baf3f3bf69ad07cdbec0f7f7
Alexandr Shaporov <a.shaporov@worksolutions.ru>
2022-05-21 10:55:50 +0300

    different syntax added

--- example.js
+++ example.js
@@ -1,6 +1,9 @@
-import { C, E, makeOverloadable, operators, Overload } from "./main.js";
+import { $, E, makeOverloadable, Overload, operators} from "./main.js";
 
-operators.forEach(({code}) => {console.log(code)})
+// console.group("operators codes:")
+// operators.forEach(({code}) => {console.log(code)})
+// console.groupEnd()
+// console.log(" ")
 
 class Wallet {
   constructor( money ) {
@@ -13,55 +16,37 @@ const w1 = new Wallet(200);
 const w2 = new Wallet(100);
 
 // Синтаксис объявления перегрузки
-Overload["+"](Wallet, Wallet, (left, right ) => {
+Overload["+"](Wallet, Wallet, ( left, right ) => {
   return left.money + right.money;
 });
 
-Overload["-"](Wallet, Wallet, (left, right) => {
+Overload["-"](Wallet, Wallet, ( left, right) => {
   return left.money - right.money;
 })
 
-Overload["+"](Wallet, Number, (left, right) => { // Когда один из аргументов примитив или массив внутри функции он доступен через arg.value, т.к. вызов возможен только через C(arg)
+Overload["+"](Wallet, Number, ( left, right) => { // Когда один из аргументов примитив или массив внутри функции он доступен через arg.value, т.к. вызов возможен только через C(arg)
   return left.money + right.value;
 })
 
-Overload["+"](Wallet, String, (l, r) => { // Когда один из аргументов примитив или массив внутри функции он доступен через arg.value, т.к. вызов возможен только через C(arg)
+Overload["+"](Wallet, String, ( l, r) => { // Когда один из аргументов примитив или массив внутри функции он доступен через arg.value, т.к. вызов возможен только через C(arg)
   return l.money + r.value
 })
 
-Overload["<<"](Array, Array, (l, r) => { // Когда один из аргументов примитив или массив внутри функции он доступен через arg.value, т.к. вызов возможен только через C(arg)
+Overload["<<"](Array, Array, ( l, r) => { // Когда один из аргументов примитив или массив внутри функции он доступен через arg.value, т.к. вызов возможен только через C(arg)
   l.value.push(...r.value);
   return l;
 })
 
-Overload["~"](Wallet, (arg) => {
-  console.log("TILDA OPERATOR");
+Overload["~"](Wallet, ( arg) => {
   return arg;
 })
 
 
 console.log("start test");
+console.log("Different syntax")
 
 console.log(
-  E(w1 + w2) // Е функция обертка, от Evaluate. Возвращает результат перегрузки. 300
-);
-
-console.log(
-  E(w1 - w2) // 100
-);
-
-console.log(
-  E(w1 + C(1000)) // 1200. Примитивы заворачиваются в C(). Внутри перегрузок значения примитива доступны через arg.value
-);
-
-console.log(
-  E(w1 + C("he")) //  Примитивы заворачиваются в C(). Внутри перегрузок значения примитива доступны через arg.value
-)
-
-console.log(
-  E(C([1,2,3]) << C([4,5,6])) //  Массивы тоже заворачиваются в C(), чтобы не изменять нативные Array.prototype.valueOf. Массив доступен внутри функции перегрузки через arg.value
-)
-
-console.log(
-  E(~w1)
+  Overload(w1 + w2),
+  E(w1+w2),
+  $(w1+w2)
 )
--- main.js
+++ main.js
@@ -71,13 +71,13 @@ export const operators = [
 ];
 
 // ИНИЦИАЛИЗАЦИЯ
-export const Overload = () => {};
-Overload.storage = {};
-Overload.resolve = ( code ) => {
+export const OverloadObserver = {};
+OverloadObserver.storage = {};
+OverloadObserver.resolve = ( code ) => {
   const {key} = operators.find(entry => entry.code === code);
-  const result = Overload.resolve[key]();
-  Overload.leftArg = null;
-  Overload.rightArg = null;
+  const result = OverloadObserver.resolve[key]();
+  OverloadObserver.leftArg = null;
+  OverloadObserver.rightArg = null;
   return result
 };
 
@@ -85,14 +85,14 @@ Overload.resolve = ( code ) => {
 operators.forEach(({key, unary}) => {
   if (unary) {
     Overload[key] = function( leftProto, overloadingFn ) {
-      Overload.storage[key].push({
+      OverloadObserver.storage[key].push({
         leftProto,
         overloadingFn
       });
     };
   } else {
     Overload[key] = function( leftProto, rightProto, overloadingFn ) {
-      Overload.storage[key].push({
+      OverloadObserver.storage[key].push({
         leftProto,
         rightProto,
         overloadingFn
@@ -100,17 +100,16 @@ operators.forEach(({key, unary}) => {
     };
   }
 
-  Overload.resolve[key] = () => {
-    const match = Overload.storage[key].find(rule => {
-      console.log(rule)
-      if ( !( Overload.leftArg instanceof rule.leftProto ) ) return false;
-      if ( !unary && !( Overload.rightArg instanceof rule.rightProto ) ) return false;
+  OverloadObserver.resolve[key] = () => {
+    const match = OverloadObserver.storage[key].find(rule => {
+      if ( !( OverloadObserver.leftArg instanceof rule.leftProto ) ) return false;
+      if ( !unary && !( OverloadObserver.rightArg instanceof rule.rightProto ) ) return false;
       return true;
     });
     if ( !match || !match.overloadingFn ) return console.error("| OVERLOAD RESOLVE FAILED | Can't find overloading function");
-    return match.overloadingFn(Overload.leftArg, Overload.rightArg);
+    return match.overloadingFn(OverloadObserver.leftArg, OverloadObserver.rightArg);
   };
-  Overload.storage[key] = [];
+  OverloadObserver.storage[key] = [];
 });
 
 /** Делаем объект доступным для перегрузок*/
@@ -121,13 +120,12 @@ export function makeOverloadable( instance ) {
 
 function exoticValueOf(instance) {
   return function (  ) {
-    console.log(instance);
-    if ( !Overload.leftArg ) {
-      Overload.leftArg = instance;
+    if ( !OverloadObserver.leftArg ) {
+      OverloadObserver.leftArg = instance;
       return MAGIC_A;
     }
-    if ( !Overload.rightArg ) {
-      Overload.rightArg = instance;
+    if ( !OverloadObserver.rightArg ) {
+      OverloadObserver.rightArg = instance;
       return MAGIC_B;
     }
     console.error("| OVERLOADING VALUE OF | CRITICAL ERROR: BUFFER IS ALREADY OCCUPIED");
@@ -136,12 +134,21 @@ function exoticValueOf(instance) {
 }
 
 /** Функция-обертка для перегрузки. В неё помещается выражение: E(obj + obj)*/
-export function E( exp ) {
-  return Overload.resolve(exp);
+export function E( e ) {
+  return OverloadObserver.resolve(e);
+}
+
+export function Overload(e) {
+  return OverloadObserver.resolve(e);
 }
 
 /** Функция-обертка, преобразует примитивное значение в overloadable объект */
-export function C( value ) {
+export function $( e ) {
+  if (OverloadObserver.leftArg && OverloadObserver.rightArg) return OverloadObserver.resolve(e)
+  return createOverloadablePrimitive(e);
+}
+
+function createOverloadablePrimitive(value) {
   const context = {
     value,
     __proto__: value.__proto__
